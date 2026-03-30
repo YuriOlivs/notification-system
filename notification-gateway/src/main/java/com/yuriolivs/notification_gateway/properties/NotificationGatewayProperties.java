9fd2ca25ea6be27b3e08dbce42102c2b4e8ed407
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-03-30 10:25:39 -0300

    feat: rename class

--- notification-gateway/src/main/java/com/yuriolivs/notification_gateway/properties/NotificationGatewayProperties.java
+++ notification-gateway/src/main/java/com/yuriolivs/notification_gateway/properties/NotificationGatewayProperties.java
@@ -4,9 +4,8 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ConfigurationProperties(prefix = "gateway")
 @Data
-public class GatewayProperties {
+public class NotificationGatewayProperties {
     private String apiKey;
 }
