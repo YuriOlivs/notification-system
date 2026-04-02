c4398285346ef23b3bc946201a571074fbf441df
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-04-02 11:30:34 -0300

    feat: apply filter chain for api key

---
+++ herald-auth/src/main/java/com/yuriolivs/herald/herald_auth/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.yuriolivs.herald.herald_auth.security;
+
+import com.yuriolivs.herald.herald_auth.security.filter.InternalKeyAuthenticationFilter;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableConfigurationProperties(SecurityConfig.class)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            InternalKeyAuthenticationFilter internalFilter
+    ) {
+        http
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(basic -> basic.disable())
+                .formLogin(form -> form.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/auth/**"
+                        )
+                        .authenticated()
+                        .anyRequest().permitAll()
+                )
+                .addFilterBefore(internalFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
